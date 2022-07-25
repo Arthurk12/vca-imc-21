87044bafde15b5ae173ef07a56feb2996007b74e
Arthurk12 <arthur_bockmann@hotmail.com>
2022-07-25 18:37:35 -0300

    remove ssh to router
    Network degradation directly on the router won't be used.

--- static.sh
+++ static.sh
@@ -6,7 +6,6 @@ CAP_INTERFACE=$3
 SHAPE_INTERFACE=$4
 TRACE=$5
 URL=$6
-ROUTER=$7
 
 
 echo "Reading trace $TRACE"
@@ -20,10 +19,8 @@ do
 		echo $line	
 		a=( $line )
 		echo "Setting ${a[1]} down and ${a[2]} up"
-		ssh -n $ROUTER "./shaper.sh start ${a[1]} ${a[2]} 0 0 $SHAPE_INTERFACE"
 		python3 test.py $APP $TIME -i $CAP_INTERFACE -id $URL -r ${a[1]}-${a[2]} 
 		sleep 1
-		ssh -n $ROUTER "./shaper.sh stop"
 		
 	done < $TRACE
 done
