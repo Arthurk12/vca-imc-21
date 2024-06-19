24ec7a73a915a9039cc6fac28b008f6ad09689f3
Arthur B. Grossi <arthur_bockmann@hotmail.com>
2024-06-18 22:01:18 -0300

    feat(limiter): add logger logs

--- limiter/limiter.py
+++ limiter/limiter.py
@@ -4,6 +4,9 @@ import csv
 import subprocess
 import time
 import socket
+from common.logger import logger
+
+LOG_PREFIX = '[LIMITER]'
 
 HOST = ''
 PORT = 6666
@@ -20,19 +23,20 @@ csv_trace = sys.argv[1]
 def apply_bandwidth_constraint(constraint_in_bytes_per_second):
   global last_applied_constraint
   if constraint_in_bytes_per_second != last_applied_constraint:
-    print(f'Applying constraint: {constraint_in_bytes_per_second} bytes per second')
+    logger.debug(f'{LOG_PREFIX} Applying constraint: {constraint_in_bytes_per_second} bytes per second')
     apply_constraint_command = f'sudo ./netspeed.sh -l {constraint_in_bytes_per_second}bps'
     subprocess.run(apply_constraint_command, shell=True)
 
     last_applied_constraint = constraint_in_bytes_per_second
 
 def clear_bandwidth_constraints():
-  print('Clearing bandwidth constraints')
+  logger.debug(f'{LOG_PREFIX} Clearing bandwidth constraints')
   clear_constraints_command = f'sudo ./netspeed.sh -s'
   subprocess.run(clear_constraints_command, shell=True)
 
-def startShaping(max_seconds):
+def start_shaping(max_seconds):
   global seconds_counter
+  logger.debug(f'{LOG_PREFIX} Called start_shaping()')
   # Abre o arquivo CSV
   with open(csv_trace, "r") as csvfile:
     reader = csv.reader(csvfile, delimiter=",")
@@ -57,24 +61,25 @@ clear_bandwidth_constraints()
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
   s.bind((HOST, PORT))
-  print(f'Listening on {HOST} port {PORT}')
+  logger.debug(f'{LOG_PREFIX} Listening on {HOST} port {PORT}')
   while True:
     s.listen()
     conn, addr = s.accept()
     with conn:
-      print(f'connected by {addr}')
+      logger.debug(f'{LOG_PREFIX}connected by {addr}')
       data = conn.recv(1024)
       if not data:
         break
       received_message = data.decode()
-      print(f'received message => {received_message}')
+      logger.debug(f'{LOG_PREFIX}received message => {received_message}')
       arguments = received_message.split('#')
       command = arguments[0]
       duration = int(float(arguments[1]))
+      logger.debug(f'{LOG_PREFIX} Identified arguments are command: {command} and duration: {duration}')
       if command == 'start':
         conn.sendall(data)
-        startShaping(duration)
+        start_shaping(duration)
       else:
-        print('received unknown message')
+        logger.error(f'{LOG_PREFIX} received unknown message!')
   time.sleep(1)
   s.close()
